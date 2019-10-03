90b2a502b7a85561ebac9a423fc33df617edb7e3
artgas1 <gasparyanartem2002@gmail.com>
2019-10-03 09:05:42 +0300

    add envs

---
+++ envs/env_windows/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\projects\project_teeth_lab\backend\envs\env_windows\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
